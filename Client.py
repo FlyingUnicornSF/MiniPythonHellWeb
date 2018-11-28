f97be5b31264724bb9f991e5f9da664a241fb919
Akiyo Marukawa <akiyo@Akiyos-MBP.dhcp.lbl.gov>
2018-11-28 14:47:01 -0800

    Client.py added

---
+++ Client.py
@@ -0,0 +1,53 @@
+import requests
+import json
+
+
+class GenericClient(object):
+    def __init__(self, module=None, url=None, token=None):
+        if module is None:
+            raise ValueError('The "module" argument is required')
+        if url is None:
+            raise ValueError('The "url" argument is required')
+
+        self.module = module
+        self.token = token
+        self.url = url
+
+    def call_func(self, func_name, params):
+        if not isinstance(func_name, basestring):
+            raise ValueError('"func_name" must be a string')
+        if not isinstance(params, list):
+            raise ValueError('"params" must be an array')
+
+        call_params = {
+            'id': 'uniquestring',
+            'method': self.module + '.' + func_name,
+            'version': '1.1',
+            'params': params
+        }
+        header = {
+            'Content-Type': 'application/json'
+        }
+        if self.token:
+            header['Authorization'] = self.token
+
+        r = requests.post(self.url, headers=header, data=json.dumps(call_params), timeout=60)
+        if r.headers.get('content-type', '').startswith('application/json'):
+            try:
+                response = r.json()
+            except Exception as err:
+                raise ValueError('Invalid response; claiming to be json, but not: %s' % err.message)
+
+            result = response.get('result')
+            error = response.get('error')
+            if error:
+                # Better be a JSON RPC 2.0 Error structure
+                raise ValueError('temp error - TODO: AKIYO')
+
+            if not (isinstance(result, list) or result is None):
+                raise ValueError('Expected result to be list, is ' + type(result).__name__)
+                
+            return result
+
+        r.raise_for_status()
+        raise ValueError('Invalid response; not json, not an error status')
--- FlaskHelloWeb.py
+++ FlaskHelloWeb.py
@@ -1,62 +1,10 @@
-from flask import Flask
-import http.server
+''' $ export FLASK_APP=FlaskHelloWeb.py
+    $ flask run'''
+from flask import Flask, url_for, request, render_template
 import os
 
 app = Flask(__name__)
 
-
-
-
-  def do_GET(self):
-    try: 
-      full_path = os.getcwd() + self.path
-
-      if not os.path.exists(full_path):
-        raise ServerException("'{0}' not found".format(self.path))
-      elif os.path.isfile(full_path):
-        self.handle_file(full_path)
-      else:
-        raise ServerException("Unknown object '{0}'".format(self.path))
-    
-    except Exception as msg:
-      self.handle_error(msg)
-
-  def handle_file(self, full_path):
-    try: 
-      with open(full_path, 'rb') as reader:
-        content = reader.read()
-      self.send_content(content)
-      
-    except IOError as msg:
-      msg = "'{0}' canoot be read: {1}".format(full_path, msg)
-      self.handle_error(msg)
-
-  Error_page = """\
-  <html>
-  <body>
-  <h1> Error accesssing {path} </h1>
-  <p>{msg}</p>
-  <html>
-  <body>
-  """
-  def handle_error(self, msg):
-    content = self.Error_page.format(path=self.path, msg=msg)
-    self.send_content(content.encode(), 404)
-
-  def send_content(self, content, status=200):
-    self.send_response(status)
-    self.send_header("Content-Type", "text/html")
-    self.send_header("Content-Length", str(len(content)))
-    self.end_headers()
-    # wfile - Contains the output stream for writing a response back to the client. 
-    # Proper adherence to the HTTP protocol must be used when writing to this stream 
-    # in order to achieve successful interoperation with HTTP clients.
-    self.wfile.write(content)
-
- #---------------------
-if __name__ == '__main__':
-  serverAddress = ('', 8080)
-  #TypeErro: getsockaddrarag: AF_INET address must be tuple, not 'str'
-  #server = http.server.HTTPServer(('', 8080), RequestHandeler)
-  server = http.server.HTTPServer(serverAddress, RequestHandeler)
-  server.serve_forever()
+@app.route('/', methods=['GET'])
+def root():
+    return render_template('index.html')
