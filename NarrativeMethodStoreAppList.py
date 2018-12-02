6573ba2dea18d42fa61fb3a6a6e15afcdca7bab5
Akiyo Marukawa <akiyo@Akiyos-MacBook-Pro.local>
2018-12-01 23:11:39 -0800

    adding drop down

--- NarrativeMethodStoreAppList.py
+++ NarrativeMethodStoreAppList.py
@@ -1,13 +1,14 @@
 ''' $ export FLASK_APP=NarrativeMethodStoreAppList.py
     $ flask run'''
-from flask import Flask, url_for, request, render_template, jsonify, Markup
+from flask import Flask, url_for, request, render_template, jsonify, Markup, g
 import os
 import requests
 import json
-
 app = Flask(__name__)
 
 _kbase_url = os.environ.get('KBASE_ENDPOINT', 'https://ci.kbase.us/services')
+# Narrative Method Store URL needs rpc at the end. 
+# ref L43/44 https://github.com/kbase/narrative_method_store/blob/master/scripts/nms-listmethods.pl 
 _NarrativeMethodStore_url = _kbase_url + '/narrative_method_store/rpc'
 payload = {
     'id': 0,
